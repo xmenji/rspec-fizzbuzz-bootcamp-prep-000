118ad63ee9133098d468d3209799cd09b61c1cab
Xochitl Menjivar <xandra.menji@gmail.com>
2018-02-06 18:42:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
   case num
-  when num / 3
+  when num  / 3 == 0
     return "Fizz"
-  when num / 5
+  when num / 5 == 0
     return "Buzz"
-  when num / 3 && num / 5
+  when num / 3 ==0 && num / 5 == 0
     return "FizzBuzz"
   else
     return nil
